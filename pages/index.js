bb17cbee10883477e9e48e36e778baceaeec94f3
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2022-09-09 16:25:45 +0200

    adding firestore to save user data

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,10 @@ export default function Index() {
     const auth = useAuth()
 
     return auth.user ? (
-        <div className="mx-auto max-w-screen-lg py-8">
-            <p className="text-lg font-bold">Email: {auth.user.email}</p>
+        <div className="mx-auto flex max-w-screen-lg flex-col items-center space-y-4 py-8">
+            <p className="text-lg font-bold text-gray-900">
+                Email: {auth.user.email}
+            </p>
             <button
                 className="rounded-md px-4 py-3 font-medium text-red-500 transition-all duration-300 ease-in-out hover:bg-red-50 hover:text-red-800"
                 onClick={(e) => auth.signout()}
