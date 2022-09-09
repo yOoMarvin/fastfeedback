41542c74eb8c32d7e4b32bf50cf27f0880738e7b
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2022-09-09 15:34:12 +0200

    initial commut + adding sign up with github

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import AuthContextProvider from '../lib/auth'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <AuthContextProvider>
+            <Component {...pageProps} />
+        </AuthContextProvider>
+    )
 }
 
 export default MyApp
